080bf5f8acfdf027c437a6cb9c30937dd66273db
Steven Kawafuchi <13loodySword@gmail.com>
2014-04-18 19:09:49 -0700

    Changed to Phagocyte

---
+++ Swarch/Assets/Swarch/Nucleus.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Nucleus : MonoBehaviour
+{
+	void Start ()
+	{
+		transform.localPosition = new Vector3(-0.08f,0.05f,0f); //I like the nucleus off-center
+	}
+	
+	void Update ()
+	{
+		transform.localPosition = new Vector3(-0.08f,0.05f,0f);
+	}
+}
+
--- Swarch/Assets/Swarch/Player.cs
+++ Swarch/Assets/Swarch/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour {
 	float size;
 	GameProcess process;
 	
+	
 	void Start () {
 		process = GameObject.Find("GameProcess").GetComponent<GameProcess>();
 		size = 0.2f;	
@@ -33,21 +34,40 @@ public class Player : MonoBehaviour {
 		}	
 	}
 	
-	void OnCollisionEnter(Collision collision) {
-		
-		//collision with walls reset score, player size, and position
-		if (collision.gameObject.name=="LeftBound" ||
-			collision.gameObject.name=="RightBound" ||
-			collision.gameObject.name=="LowerBound" ||
-			collision.gameObject.name=="UpperBound"){
-			
+	//Currently checks if player has completely exited the circular bounds of the slide; 
+	//need to find a way to check if a point of the player's Box Collider is not contained
+	//within the slide's Capsule Collider.
+	void OnCollisionExit(Collision collision)
+	{
+		if(collision.gameObject.name == "MicroscopeSlide")
+		{
 			process.score = 0;
 			transform.position = new Vector3(Random.Range (2,25),Random.Range (-18,18),0);
 			size = 0.20f;
 			transform.localScale = new Vector3(1,1,1);
-			
+		}
+	}
+	
+//	void OnCollisionStay(Collision collision)
+//	{
+//		if(collision.gameObject.name == "MicroscopeSlide")
+//		{
+//			if(collision.collider.bounds.Intersects(collider.bounds))
+//			{
+//				process.score = 0;
+//				transform.position = new Vector3(Random.Range (2,25),Random.Range (-18,18),0);
+//				size = 0.20f;
+//				transform.localScale = new Vector3(1,1,1);
+//			}
+//		}
+//	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.name == process.pellets[0].name || collision.gameObject.name == process.pellets[1].name ||
+			collision.gameObject.name == process.pellets[2].name || collision.gameObject.name == process.pellets[3].name ||
+			collision.gameObject.name == process.pellets[4].name)
+		{
 		//collision with the pellets
-		}else{
 			//update score
 			process.score++;
 			
@@ -58,8 +78,9 @@ public class Player : MonoBehaviour {
 			transform.localScale = new Vector3(width*1.2f,width*1.2f,0);
 			
 			//Moves pellet that was hit to a new position
-			process.pellets[int.Parse(collision.gameObject.name)].transform.position = new Vector3(Random.Range (2,25),Random.Range (-18,18),0);
-			
+			process.pellets[int.Parse(collision.gameObject.name)].transform.position = new Vector3(Random.Range (-2,18),Random.Range (-10,10),0);
+			process.pelletShadows[int.Parse(collision.gameObject.name)].transform.position = process.pellets[int.Parse(collision.gameObject.name)].transform.position;
 		}
-    }
+	}
+	
 }
