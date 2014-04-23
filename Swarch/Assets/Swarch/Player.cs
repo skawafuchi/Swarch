98f071657fc2fe11e8cb0b75f13ff098d7b6ca7e
Tamara Sahagun <tbsahagun@hotmail.com>
2014-04-22 17:08:40 -0700

    Finally fixed collision detection at bounds!
    I modeled a cylinder in Blender so that it would be the same size as the
    slide, and inverted the normals. After it was imported into Unity the
    code was simple; just check in OnCollisionEnter()

--- Swarch/Assets/Swarch/Player.cs
+++ Swarch/Assets/Swarch/Player.cs
@@ -7,23 +7,17 @@ public class Player : MonoBehaviour {
 	GameProcess process;
 	Vector3[] colliderVertices = new Vector3[8]; 
 	public static float slideX, slideY;
+	public static Vector3 slideCenter;
 	
 	void Start () {
 		process = GameObject.Find("GameProcess").GetComponent<GameProcess>();
 		size = 0.2f;	
 		transform.position = new Vector3(Random.Range (2,25),Random.Range (-18,18),0);
 		
-		colliderVertices[0] = collider.bounds.min;
-		colliderVertices[1] = collider.bounds.max;
-		colliderVertices[2] = new Vector3(colliderVertices[0].x, colliderVertices[0].y, colliderVertices[1].z);
-		colliderVertices[3] = new Vector3(colliderVertices[0].x, colliderVertices[1].y, colliderVertices[0].z);
-		colliderVertices[4] = new Vector3(colliderVertices[1].x, colliderVertices[0].y, colliderVertices[0].z);
-		colliderVertices[5] = new Vector3(colliderVertices[0].x, colliderVertices[1].y, colliderVertices[1].z);
-		colliderVertices[6] = new Vector3(colliderVertices[1].x, colliderVertices[0].y, colliderVertices[1].z);
-		colliderVertices[7] = new Vector3(colliderVertices[1].x, colliderVertices[1].y, colliderVertices[0].z);
-		
+		slideCenter = GameObject.Find("MicroscopeSlide").transform.renderer.bounds.center;
 		slideX = GameObject.Find("MicroscopeSlide").transform.renderer.bounds.center.x;
 		slideY = GameObject.Find("MicroscopeSlide").transform.renderer.bounds.center.y;
+		
 	}
 	
 	// Update is called once per frame
@@ -45,45 +39,16 @@ public class Player : MonoBehaviour {
 			xdir = 1;
 			ydir = 0;
 		}	
+		
+		colliderVertices[0] = collider.bounds.min;
+		colliderVertices[1] = collider.bounds.max;
+		colliderVertices[2] = new Vector3(colliderVertices[0].x, colliderVertices[0].y, colliderVertices[1].z);
+		colliderVertices[3] = new Vector3(colliderVertices[0].x, colliderVertices[1].y, colliderVertices[0].z);
+		colliderVertices[4] = new Vector3(colliderVertices[1].x, colliderVertices[0].y, colliderVertices[0].z);
+		colliderVertices[5] = new Vector3(colliderVertices[0].x, colliderVertices[1].y, colliderVertices[1].z);
+		colliderVertices[6] = new Vector3(colliderVertices[1].x, colliderVertices[0].y, colliderVertices[1].z);
+		colliderVertices[7] = new Vector3(colliderVertices[1].x, colliderVertices[1].y, colliderVertices[0].z);
 	}
-	
-	//Currently checks if player has completely exited the circular bounds of the slide; 
-	//need to find a way to check if a point of the player's Box Collider is not contained
-	//within the slide's Capsule Collider.
-	void OnTriggerExit(Collider otherCollider)
-	{
-		if(otherCollider.gameObject.name == "MicroscopeSlide")
-		{
-			print("No longer in contact with " + otherCollider.transform.name);
-			process.score = 0;
-			transform.position = new Vector3(Random.Range (-2,18),Random.Range (-10,10),0);
-			while(!IsThisPointWithinBounds(transform.position))
-			{
-				transform.position = new Vector3(Random.Range (-2,18),Random.Range (-10,10),0);
-			}
-			size = 0.20f;
-			transform.localScale = new Vector3(1,1,1);
-		}
-	}
-	
-//	void OnTriggerStay(Collider otherCollider)
-//	{
-//		if(otherCollider.gameObject.name == "MicroscopeSlide")
-//		{
-//			
-//			for(int i = 0; i < 8; i++)
-//			{
-//				if(!IsThisPointWithinBounds(colliderVertices[i]))
-//				{
-//					print("Math rules!");
-//					process.score = 0;
-//					transform.position = new Vector3(Random.Range (-2,18),Random.Range (-10,10),0);
-//					size = 0.20f;
-//					transform.localScale = new Vector3(1,1,1);
-//				}
-//			}
-//		}
-//	}
 	
 	void OnCollisionEnter(Collision collision) {
 		if(collision.gameObject.name == process.pellets[0].name || collision.gameObject.name == process.pellets[1].name ||
@@ -109,11 +74,24 @@ public class Player : MonoBehaviour {
 			}
 			process.pelletShadows[int.Parse(collision.gameObject.name)].transform.position = process.pellets[int.Parse(collision.gameObject.name)].transform.position;
 		}
+		
+		if(collision.gameObject.name == "Cylinder")
+		{
+			print ("Touched edge!");
+			process.score = 0;
+			transform.position = new Vector3(Random.Range (-2,18),Random.Range (-10,10),0);
+			while(!IsThisPointWithinBounds(transform.position))
+			{
+				transform.position = new Vector3(Random.Range (-2,18),Random.Range (-10,10),0);
+			}
+			size = 0.20f;
+			transform.localScale = new Vector3(1,1,1);
+		}
 	}
 				
 	public static bool IsThisPointWithinBounds(Vector3 vector)
 	{
-		if((Mathf.Pow((vector.x - slideX),2f) + Mathf.Pow((vector.y - slideY),2f)) < 400)
+		if((Mathf.Pow((vector.x - slideX),2f) + Mathf.Pow((vector.y - slideY),2f)) < 400f)
 			return true;
 		return false;
 	}
