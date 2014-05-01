70f6d9d58ade450062f44f27348fd4a530cce937
Tamara Sahagun <tbsahagun@hotmail.com>
2014-04-30 22:20:39 -0700

    SQLite password checking/database lookup implemented
    I kept the server running while I opened and closed the Unity executable
    several times attempting to login with the same username; if I entered
    the incorrect password, nothing would happen (other than printing a
    message to the server console) until I entered the correct password.

--- Server/Server/Phagocyte.cs
+++ Server/Server/Phagocyte.cs
@@ -74,45 +74,88 @@ namespace Server
                             System.Buffer.BlockCopy(read, indexOfParse + 1, password, 0, msgSize - indexOfParse - 1);
                             string strUN = Encoding.ASCII.GetString(userName);
                             string strPW = Encoding.ASCII.GetString(password);
+                            //Need to trim off whitespace
+                            strUN = strUN.TrimEnd('\0');
+                            strPW = strPW.TrimEnd('\0');
                             Console.WriteLine("username: " + strUN + "password: " + strPW);
 
-                            //For now, add player to table without checking
-                            //Password will not be encrypted yet
-                            string sql = "INSERT INTO playerData (username, password, score) VALUES (@username, @password, 0)";
-                            SQLiteCommand command = new SQLiteCommand(sql, Server.p_dbConnection);
-                            command.Parameters.AddWithValue("@username", strUN);
-                            command.Parameters.AddWithValue("@password", strPW);
-                            command.ExecuteNonQuery();
-
-                            //Print out table for testing
-                            sql = "SELECT * FROM playerData";
-                            command = new SQLiteCommand(sql, Server.p_dbConnection);
-                            SQLiteDataReader reader = command.ExecuteReader();
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("Username: " + reader["username"] + " | Password: " 
-                                    + reader["password"] + " | Score: " + reader["score"]);
-                            }
-
-                            //Check to see if database
-                            //NEEDS IMPLEMENTATION
-                            //if(IN DATABASE)
                             byte[] toSend = new byte[50];
                             //toSend[1] is 0 if they are not allowed access, 1 if allowed
                             //currently always allowing players to access
-                            toSend[1] = 1;
 
+                            //Check to see if player is in database
+                            Console.WriteLine("1");
+                            string sql = "SELECT COUNT(*) FROM playerData WHERE username=@username";
+                            Console.WriteLine("2");
+                            SQLiteCommand command = new SQLiteCommand(sql, Server.p_dbConnection);
+                            Console.WriteLine("3");
+                            command.Parameters.AddWithValue("@username", strUN);
+                            Console.WriteLine("4");
+                            int count = Convert.ToInt32(command.ExecuteScalar());
+                            Console.WriteLine("match = " + count);
+                            if (count > 0)
+                            {
+                                Console.WriteLine("Checking...");
+                                //Check if passwords match
+                                string compare = "";
+                                sql = "SELECT password FROM playerData WHERE username=@username";
+                                command = new SQLiteCommand(sql, Server.p_dbConnection);
+                                command.Parameters.AddWithValue("@username", strUN);
+                                Console.WriteLine("5");
+                                SQLiteDataReader reader2 = command.ExecuteReader();
+                                Console.WriteLine("6");
+                                while (reader2.Read())
+                                {
+                                    Console.WriteLine("7");
+                                    compare = (string)reader2["password"];
+                                    Console.WriteLine("8");
+                                    
+                                }
+                                Console.WriteLine("strPw = " + strPW + "| compare = " + compare);
+                                if (strPW.Equals(compare))
+                                {
+                                    toSend[1] = 1;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter the correct password.");
+                                }
+                            }
+                            else //Add player to table
+                            {
+                                //Password will not be encrypted yet
+                                sql = "INSERT INTO playerData (username, password, score) VALUES (@username, @password, 0)";
+                                command = new SQLiteCommand(sql, Server.p_dbConnection);
+                                command.Parameters.AddWithValue("@username", strUN);
+                                command.Parameters.AddWithValue("@password", strPW);
+                                command.ExecuteNonQuery();
+
+                                //Print out table for testing
+                                sql = "SELECT * FROM playerData";
+                                command = new SQLiteCommand(sql, Server.p_dbConnection);
+                                SQLiteDataReader reader3 = command.ExecuteReader();
+                                while (reader3.Read())
+                                {
+                                    Console.WriteLine("Username: " + reader3["username"] + " | Password: "
+                                        + reader3["password"] + " | Score: " + reader3["score"]);
+                                }
+
+                                toSend[1] = 1;
+                           }
                             //if they connected
                             //sends player pellet info
-                            int counter = 0;
-                            for (int i = 2; i <= Server.pellets.Count*2; i+=2) {
-                                toSend[i] = (byte)(Server.pellets[i - (2 + counter)].x);
-                                toSend[i + 1] = (byte)(Server.pellets[i-(2+counter)].y);
-                                counter++;
+                            if (toSend[1] == 1)
+                            {
+                                int counter = 0;
+                                for (int i = 2; i <= Server.pellets.Count * 2; i += 2)
+                                {
+                                    toSend[i] = (byte)(Server.pellets[i - (2 + counter)].x);
+                                    toSend[i + 1] = (byte)(Server.pellets[i - (2 + counter)].y);
+                                    counter++;
+                                }
+
+                                netStream.Write(toSend, 0, 50);
                             }
-
-                            netStream.Write(toSend, 0, 50);
-
                         }
                         //first byte is a 1 then it's a move command
                         else if (read[0] == 1)
